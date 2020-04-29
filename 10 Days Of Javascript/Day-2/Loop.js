f8f9780439c1bd061de1cc9a99d2b6bbf3f30a6b
hackytech1234 <54655323+hackytech1234@users.noreply.github.com>
2020-04-29 22:54:19 +0530

    Update Loop.js

--- 10 Days Of Javascript/Day-2/Loop.js
+++ 10 Days Of Javascript/Day-2/Loop.js
@@ -24,3 +24,29 @@ function vowelsAndConsonants(s) {
     }
 }
 vowelsAndConsonants("javascriptloops");
+
+
+##############################################################################################################################
+SECOND SOLUTION :- 
+   
+function vowelsAndConsonants(s) {
+    var vowels = 'a e i o u'
+    var i;
+    for(i =0; i<=s.length-1;i++){
+        if (vowels.indexOf(s.charAt(i)) != -1){
+            console.log(s.charAt(i))
+        }
+    }
+    for (i=0; i<=s.length-1; i++){
+        if (vowels.indexOf(s.charAt(i)) == -1){
+            console.log(s.charAt(i))
+        }
+    }
+}
+
+
+function main() {
+    const s = readLine();
+    
+    vowelsAndConsonants(s);
+}
